6ce589715e8c71402bb665bce96a55b34d413ac1
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-12-23 22:41:33 +0200

    update my knowledge about this project :)

--- my-app/src/services/topicService.ts
+++ my-app/src/services/topicService.ts
@@ -13,7 +13,7 @@ export const topicService = createApi({
         getTopics: builder.query<IParentTopic[], void>({
             query: () => {
                 return {
-                    url: '/all',
+                    url: '/',
                     method: 'GET'
                 };
             },
